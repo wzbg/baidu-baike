d48e406eb27dff16c80e099629b56906a0f7e197
zyc <z35305515@gmail.com>
2016-02-23 17:51:07 +0800

    fetchUrl

--- index.js
+++ index.js
@@ -2,70 +2,75 @@
 * @Author: zyc
 * @Date:   2016-02-18 14:06:33
 * @Last Modified by:   zyc
-* @Last Modified time: 2016-02-23 14:22:11
+* @Last Modified time: 2016-02-23 17:44:44
 */
 'use strict'
 
+const fetchUrl = require('fetch').fetchUrl
 const request = require('sync-request')
 const cheerio = require('cheerio')
 const URL = require('url')
 
 const url = 'http://baike.baidu.com/item/'
 
-module.exports = query => {
-  const res = request('GET', url + encodeURI(query))
-  const finalUrl = decodeURI(res.url)
-  const $ = cheerio.load(res.body.toString())
-  $('script,sup,.description,.album-list').remove() // 删除参考资料 & 描述 & 词条图册
-  const result = {
-    finalUrl, // 最终网址
-    name: $('dd.lemmaWgt-lemmaTitle-title h1').text(), // 名称 
-    item: $('dd.lemmaWgt-lemmaTitle-title h2').text(), // 义项
-    summary: $('div.lemma-summary').text().trim(), // 概要
-    images: getPic($('div.summary-pic a').attr('href')), // 概要相册
-    contents: [] // 内容
-  }
-  $('div.basic-info dl dt').each((index, element) => {
-    result.basicInfo = result.basicInfo || [] // 基本信息
-    result.basicInfo.push({
-      name: $(element).text().replace(/\s+/g, ''),
-      value: $(element).next().text().replace(/\s+/g, '')
+module.exports = query => (
+  new Promise((resolve, reject) => {
+    fetchUrl(url + encodeURI(query), (err, res, buf) => {
+      const finalUrl = decodeURI(res.finalUrl)
+      if (finalUrl.endsWith('/error.html')) return reject(new Error('not found'))
+      const $ = cheerio.load(buf)
+      $('script,sup,.description,.album-list').remove() // 删除参考资料 & 描述 & 词条图册
+      const result = {
+        finalUrl, // 最终网址
+        name: $('dd.lemmaWgt-lemmaTitle-title h1').text(), // 名称 
+        item: $('dd.lemmaWgt-lemmaTitle-title h2').text(), // 义项
+        summary: $('div.lemma-summary').text().trim(), // 概要
+        images: getPic($('div.summary-pic a').attr('href')), // 概要相册
+        contents: [] // 内容
+      }
+      $('div.basic-info dl dt').each((index, element) => {
+        result.basicInfo = result.basicInfo || [] // 基本信息
+        result.basicInfo.push({
+          name: $(element).text().replace(/\s+/g, ''),
+          value: $(element).next().text().replace(/\s+/g, '')
+        })
+      })
+      $('li.item a,li.item span').each((index, element) => {
+        const node = $(element)
+        const href = node.attr('href')
+        result.items = result.items || [] // 义项
+        result.items.push({
+          name: node.text(),
+          url: href ? URL.resolve(url, href) : finalUrl,
+          current: href ? false : true
+        })
+      })
+      $('h2.para-title').each((index, element) => {
+        const title = $(element).find('span.title-text').text()
+        const content = []
+        for (let node =  $(element).next(); node.get(0) && node.get(0).name !== 'h2'; node = node.next()) {
+          const para = getPara($, node)
+          if (para) content.push(para)
+        }
+        result.contents.push({ title, content })
+      })
+      if (!result.contents.length) {
+        const title = ''
+        const content = []
+        $('div.para').each((index, element) => {
+          const para = getPara($, $(element))
+          if (para) content.push(para)
+        })
+        result.contents.push({ title, content })
+      }
+      $('span.taglist').each((index, element) => {
+        result.tags = result.tags || [] // 词条标签
+        result.tags.push($(element).text().replace(/\s+/g, ''))
+      })
+      resolve(result)
     })
   })
-  $('li.item a,li.item span').each((index, element) => {
-    const node = $(element)
-    const href = node.attr('href')
-    result.items = result.items || [] // 义项
-    result.items.push({
-      name: node.text(),
-      url: href ? URL.resolve(url, href) : finalUrl,
-      current: href ? false : true
-    })
-  })
-  $('h2.para-title').each((index, element) => {
-    const title = $(element).find('span.title-text').text()
-    const content = []
-    for (let node =  $(element).next(); node.get(0) && node.get(0).name !== 'h2'; node = node.next()) {
-      const para = getPara($, node)
-      if (para) content.push(para)
-    }
-    result.contents.push({ title, content })
-  })
-  if (!result.contents.length) {
-    const title = ''
-    const content = []
-    $('div.para').each((index, element) => {
-      const para = getPara($, $(element))
-      if (para) content.push(para)
-    })
-    result.contents.push({ title, content })
-  }
-  $('span.taglist').each((index, element) => {
-    result.tags = result.tags || [] // 词条标签
-    result.tags.push($(element).text().replace(/\s+/g, ''))
-  })
-  return result
-}
+)
 
 const getPara = ($, node) => {
   const text = node.text().replace(/\s+/g, '')
--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 * @Author: zyc
 * @Date:   2016-02-18 14:39:14
 * @Last Modified by:   zyc
-* @Last Modified time: 2016-02-23 14:26:25
+* @Last Modified time: 2016-02-23 17:48:25
 */
 'use strict'
 
@@ -11,17 +11,7 @@ const baike = require('./index')
 // const query = 'seed'
 // const query = '科技'
 // const query = '太空'
-const query = '三体'
+// const query = '三体'
+const query = 'Twitter'
 
-const result = baike(query)
-console.log(result)
-
-for (let content of result.contents) {
-  console.log(content.title, content.content)
-  // console.log(content.title)
-  // for (let con of content.content) {
-  //   if (con.table) {
-  //     console.log(con.type, con.table)
-  //   }
-  // }
-}
+baike(query).then(res => console.log(res)).catch(err => console.error(err))
