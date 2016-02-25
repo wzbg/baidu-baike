a3c59cea0d368297d2d2d8eb9a377de55ea36026
zyc <z35305515@gmail.com>
2016-02-25 13:26:39 +0800

    Error: incorrect header check

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 * @Author: zyc
 * @Date:   2016-02-18 14:06:33
 * @Last Modified by:   zyc
-* @Last Modified time: 2016-02-25 13:14:14
+* @Last Modified time: 2016-02-25 13:25:32
 */
 'use strict'
 
@@ -106,22 +106,27 @@ const getPara = ($, node) => {
 }
 
 const getPic = href => {
-  if (!href) return
-  const res = request('GET', URL.resolve(url, href))
-  const $ = cheerio.load(res.body.toString())
   const pics = new Set()
-  pics.add($('a.origin').attr('href'))
-  if (href.endsWith('ct=cover')) {
-    $('a.pic-item[href]').each((index, element) => {
-      const origUrl = $(element).attr('href')
-      try {
-        const res = request('GET', URL.resolve(url, origUrl))
-        const $$ = cheerio.load(res.body.toString())
-        pics.add($$('a.origin').attr('href'))
-      } catch (err) {
-        // console.error(origUrl, err)
+  try {
+    if (href) {
+      const res = request('GET', URL.resolve(url, href))
+      const $ = cheerio.load(res.body.toString())
+      pics.add($('a.origin').attr('href'))
+      if (href.endsWith('ct=cover')) {
+        $('a.pic-item[href]').each((index, element) => {
+          const origUrl = $(element).attr('href')
+          try {
+            const res = request('GET', URL.resolve(url, origUrl))
+            const $$ = cheerio.load(res.body.toString())
+            pics.add($$('a.origin').attr('href'))
+          } catch (err) {
+            // console.error(origUrl, err)
+          }
+        })
       }
-    })
+    }
+  } catch (err) {
+    // console.error(href, err)
   }
   return Array.from(pics)
 }
